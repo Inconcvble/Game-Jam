2fb9a1da8104401b7256d57f659338e82fedc661
Inconcvble <141197961+Inconcvble@users.noreply.github.com>
2026-03-07 17:21:58 +0200

    Just ensuring folders are around

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player {
+    public class PlayerMovement : MonoBehaviour {
+        
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private InputAction playerControls;
+        [SerializeField] private float moveSpeed;
+        [SerializeField] private Vector2 moveDirection;
+
+        void Start() {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            moveDirection.x = playerControls.ReadValue<Vector2>().x;
+            moveDirection.y = playerControls.ReadValue<Vector2>().y;
+        }
+
+        void FixedUpdate() {
+            rb.linearVelocity = new Vector2(moveDirection.x * moveSpeed * Time.deltaTime, moveDirection.y * moveSpeed * Time.deltaTime);
+        }
+
+        private void OnEnable() {
+            playerControls.Enable();
+        }
+
+        private void OnDisable() {
+            playerControls.Disable();
+        }
+    }
+}
